c86f7e6c934c0ab3f099cf645518e5b85f11d528
frank931023 <frank931023@gmail.com>
2025-06-04 03:38:42 +0800

    fix: add production origin to CORS configuration

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,10 @@ app = Flask(__name__, static_folder="dist", static_url_path="/")
 CORS(app, 
      resources={
          r"/*": {  # 允許所有路由
-             "origins": ["http://localhost:8080", "http://localhost:8081"],
+             "origins": ["http://localhost:8080", 
+                         "http://localhost:8081", 
+                         "https://mind-map-3ckr.onrender.com"
+                         ],
              "methods": ["GET", "POST", "OPTIONS"],
              "allow_headers": ["Content-Type"],
              "supports_credentials": True,
